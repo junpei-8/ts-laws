bfc422e494be54fbf59ed32c308c97e0c52e04d4
junpei-8 <miurajunpeis2@gmail.com>
2023-06-24 09:33:27 +0900

    update: リント・フォーマッターの設定更新

--- vue/.eslintrc.cjs
+++ vue/.eslintrc.cjs
@@ -1,15 +1,132 @@
-/* eslint-env node */
+/** @see https://github.com/microsoft/rushstack#readme */
 require('@rushstack/eslint-patch/modern-module-resolution');
 
+const COMMON_EXTENSIONS = 'cjs,cts';
+const TS_EXTENSIONS = 'ts,cts,mts';
+
+/** @type {import('eslint/lib/shared/types').ConfigData} */
 module.exports = {
-	root: true,
-	extends: [
-		'plugin:vue/vue3-essential',
-		'eslint:recommended',
-		'@vue/eslint-config-typescript/recommended',
-		'@vue/eslint-config-prettier/skip-formatting',
-	],
-	parserOptions: {
-		ecmaVersion: 'latest',
-	},
+  root: true,
+
+  parserOptions: {
+    ecmaVersion: 'latest',
+    warnOnUnsupportedTypeScriptVersion: false,
+  },
+
+  plugins: [
+    /** @npm https://www.npmjs.com/package/eslint-plugin-import */
+    'import',
+
+    /** @npm https://www.npmjs.com/package/eslint-plugin-sort-keys-custom-order */
+    'sort-keys-custom-order',
+  ],
+
+  extends: [
+    /**
+     * @npm https://www.npmjs.com/package/eslint-plugin-vue
+     * @config https://github.com/vuejs/eslint-plugin-vue/blob/master/lib/index.js
+     */
+    'plugin:vue/vue3-essential',
+
+    /** @config https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/src/configs/recommended.ts */
+    'eslint:recommended',
+
+    /**
+     * @npm https://www.npmjs.com/package/@vue/eslint-config-prettier
+     * @docs https://github.com/vuejs/eslint-config-prettier#use-separate-commands-for-linting-and-formatting
+     * @config https://github.com/vuejs/eslint-config-prettier/blob/main/skip-formatting.js
+     */
+    '@vue/eslint-config-prettier/skip-formatting',
+  ],
+
+  rules: {
+    /** @docs https://eslint.org/docs/latest/rules */
+    'no-console': 'off',
+    'no-debugger': 'off',
+
+    /** @docs https://github.com/import-js/eslint-plugin-import#rules */
+    'import/no-duplicates': ['warn', { considerQueryString: true }],
+    'import/no-unresolved': 'off',
+    'import/namespace': 'off',
+    'import/named': 'off',
+    'import/order': [
+      'warn',
+      {
+        groups: [
+          'builtin',
+          'internal',
+          'external',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+        ],
+        pathGroups: [
+          {
+            pattern: '~/**',
+            group: 'external',
+            position: 'after',
+          },
+          {
+            pattern: '@/**',
+            group: 'external',
+            position: 'after',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['builtin'],
+        alphabetize: { order: 'asc' },
+        'newlines-between': 'never',
+      },
+    ],
+
+    /** @docs https://github.com/hugoattal/eslint-plugin-sort-keys-custom-order#usage */
+    'sort-keys-custom-order/import-object-keys': 'warn',
+    'sort-keys-custom-order/export-object-keys': 'warn',
+  },
+
+  overrides: [
+    {
+      files: [`**/*.{${COMMON_EXTENSIONS}}`],
+
+      env: { node: true },
+    },
+
+    {
+      files: [`**/*.{${TS_EXTENSIONS},tsx,vue}`],
+
+      extends: [
+        /**
+         * @npm https://www.npmjs.com/package/@vue/eslint-config-typescript
+         * @config https://github.com/vuejs/eslint-config-typescript/blob/main/recommended.js
+         */
+        '@vue/eslint-config-typescript/recommended',
+      ],
+
+      settings: {
+        /** @docs https://github.com/import-js/eslint-import-resolver-typescript#configuration */
+        'import/resolver': { typescript: {} },
+      },
+
+      rules: {
+        /** @docs https://typescript-eslint.io/rules */
+        '@typescript-eslint/no-namespace': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+        '@typescript-eslint/no-empty-interface': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/no-import-type-side-effects': 'error',
+        '@typescript-eslint/consistent-type-imports': [
+          'error',
+          {
+            prefer: 'type-imports',
+            fixStyle: 'inline-type-imports',
+            disallowTypeAnnotations: true,
+          },
+        ],
+
+        /** @docs https://github.com/import-js/eslint-plugin-import#rules */
+        'import/consistent-type-specifier-style': ['warn', 'prefer-top-level'],
+      },
+    },
+  ],
 };
--- vue/.prettierrc.cjs
+++ vue/.prettierrc.cjs
@@ -1,11 +1,11 @@
 /** @type {import('prettier').Config} */
 module.exports = {
   plugins: [
-    /** @npm https://www.npmjs.com/package/prettier-plugin-organize-attributes */
-    require.resolve('prettier-plugin-organize-attributes'),
-
     /** @npm https://www.npmjs.com/package/prettier-plugin-jsdoc */
     require.resolve('prettier-plugin-jsdoc'),
+
+    /** @npm https://www.npmjs.com/package/prettier-plugin-organize-attributes */
+    require.resolve('prettier-plugin-organize-attributes'),
   ],
 
   /** @docs https://prettier.io/docs/en/options */
@@ -13,17 +13,6 @@ module.exports = {
   semi: true,
   singleQuote: true,
   trailingComma: 'all',
-  htmlWhitespaceSensitivity: 'ignore',
-
-  /** @docs https://github.com/NiklasPor/prettier-plugin-organize-attributes#usage */
-  attributeGroups: [
-    '^id$',
-    '^class$',
-    '^name$',
-    '$DEFAULT',
-    '^data-',
-    '^aria-',
-  ],
 
   /** @docs https://github.com/hosseinmd/prettier-plugin-jsdoc#options */
   tsdoc: true,
@@ -32,8 +21,40 @@ module.exports = {
   jsdocPreferCodeFences: true,
   jsdocKeepUnParseAbleExampleIndent: true,
 
-  /** @docs https://github.com/withastro/prettier-plugin-astro#configuration */
-  astroAllowShorthand: false,
+  /**
+   * @docs https://github.com/NiklasPor/prettier-plugin-organize-attributes#usage
+   * @refs https://vuejs.org/style-guide/rules-recommended.html#element-attribute-order
+   */
+  attributeGroups: [
+    '^(is|:is)$',
+    '^v-for$',
+    '^v-if$',
+    '^v-else-if$',
+    '^v-else$',
+    '^v-show$',
+    '^v-cloak$',
+    '^v-pre$',
+    '^v-once$',
+    '^v-slot$',
+    '^(ref|:ref)$',
+    '^(key|:key)$',
+    '^(id|:id)$',
+    '^(for|:for)$',
+    '^(name|:name)$',
+    '^(class|:class)$',
+    '$DEFAULT',
+    '^aria-',
+    '^data-',
+    '^v-model$',
+    '^v-model:',
+    '^v-bind$',
+    '^v-bind:',
+    '^:',
+    '^@',
+    '^v-on:',
+    '^v-html',
+    '^v-text',
+  ],
 
   /** @docs https://prettier.io/docs/en/configuration.html#configuration-overrides */
   overrides: [{ files: ['**/*.svg'], options: { parser: 'html' } }],
